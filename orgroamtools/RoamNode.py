de98d1ed9afe57b5b9d03173ea7e728ed2d05359
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-12-13 22:44:39 -0600

    Type hints and name changes

--- orgroamtools/RoamNode.py
+++ orgroamtools/RoamNode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+from typing import Iterable
 
 class RoamNode:
     """Node for org-roam zettels
@@ -13,7 +14,11 @@ class RoamNode:
     links_to -- set of ids this node links in its body
     """
 
-    def __init__(self, fname, title, id, tags, links_to):
+    def __init__(self, fname : str,
+                 title : str,
+                 id : str,
+                 tags : set[str],
+                 links_to : set[str]):
         """
         Params
         fname -- filename of node locatoin
@@ -23,26 +28,32 @@ class RoamNode:
         links_to -- set of ids this node links in its body
         """
         super(RoamNode, self).__init__()
-        self.fname = fname
-        self.title = title
-        self.id = id
-        self.tags = tags
-        self.links_to = links_to
+        self._fname = fname
+        self._title = title
+        self._id = id
+        self._tags = tags
+        self._links_to = links_to
 
     @property
-    def links(self, other_id):
+    def links(self, other_id : str):
         """
         Returns set of tags this node links to
         """
-        return other_id in self.links_to
+        return other_id in self._links_to
 
+    @property
+    def fname(self) -> str:
+        return self._fname
+
+    @property
     def tags(self):
         """
         Returns node tags
         """
-        return self.tags
+        return self._tags
 
-    def links_to(self, n, directed=False):
+
+    def links_to(self, n , directed : bool = False):
         """
         Determined if node links to another node
 
@@ -57,32 +68,32 @@ class RoamNode:
         """
 
         if directed:
-            return n.id in self.links_to
+            return n._id in self._links_to
         else:
-            return n.id in self.links_to or self.id in n.links_to
+            return n._id in self._links_to or self._id in n._links_to
 
     @property
-    def get_id(self):
+    def id(self):
         """
         Returns node id
         """
-        return self.id
+        return self._id
 
     @property
-    def get_links(self):
+    def links(self) -> set[str]:
         """
         Returns links of a node
         """
-        return self.links_to
+        return self._links_to
 
     @property
-    def get_title(self):
+    def title(self) -> str:
         """
         Returns title of node
         """
-        return self.title
+        return self._title
 
-    def has_tag(self, tags_checked):
+    def has_tag(self, tags_checked : Iterable[str]) -> bool:
         """
         Checks if node has tag
 
@@ -94,12 +105,12 @@ class RoamNode:
         """
         return any(tag in tags_checked for tag in self.tags)
 
-    def has_regex_tag(self, rxs):
+    def has_regex_tag(self, rxs : Iterable[str]) -> bool:
         """
         Checks if node has regex tag
 
         Params
-        rxs -- iterable (compiled regexes)
+        rxs -- iterable
             Iterable of regexes to match
 
         Returns True if node tag matches any of the regexes
@@ -107,19 +118,19 @@ class RoamNode:
         return any(rx.match(tag) for tag in self.tags for rx in rxs)
 
     def __str__(self):
-        return f"({self.title}, {self.id})"
+        return f"({self._title}, {self._id})"
 
     def __repr__(self):
-        return f"({self.title}, {self.id})"
+        return f"({self._title}, {self._id})"
 
-    def __lt__(self,other):
+    def __lt__(self,other) -> bool:
         if not isinstance(other, RoamNode):
             return NotImplemented
 
-        self.id < other.id
+        self._id < other.id
 
 
-    def __gt__(self,other):
+    def __gt__(self,other) -> bool:
         if not isinstance(other, RoamNode):
             return NotImplemented
 
