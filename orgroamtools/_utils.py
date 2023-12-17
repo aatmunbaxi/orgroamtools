d152b83d661a20426fb6384f456c0f61f0d7c4b6
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-12-17 11:32:12 -0600

    Introduce interface for miscellaneous org-links

--- orgroamtools/_utils.py
+++ orgroamtools/_utils.py
@@ -1,9 +1,33 @@
 import re
 from enum import Enum
+from dataclasses import dataclass
+from typing import Optional
 
 ORG_ID_FORMAT = re.compile(
     r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
 )
+
+
+# TODO Maybe consolidate all links to this class for richer representation of links?
+@dataclass
+class MiscLink:
+    """
+    Miscellaneous link type for other org-mode type links
+
+    Attributes
+    ----------
+    type : str
+        the type of linke (e.g. https, fil)
+    content : str
+        content of the link
+    desc : Optional[str]
+        description of the link
+    """
+    type : str
+    content : str
+    desc : Optional[str]
+
+
 
 
 class IdentifierType(Enum):
@@ -18,9 +42,15 @@ class IdentifierType(Enum):
     NOTHING = 0
 
 
+
+
 class DuplicateTitlesWarning(Warning):
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
         return repr(self.message)
+
+
+def parse_orglink(link_body : str) -> MiscLink:
+    pass
