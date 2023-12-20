8eb17be9911ea43da6ec0663c1d4edcb4476463a
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-12-19 18:10:29 -0600

    km

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os, sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'orgroamtools'
 copyright = '2023, abaxi'
@@ -34,4 +34,4 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_output_dir = "build/html"
-html_static_path = ['_static']
+html_static_path = ['build/html/_static']
