d06297a88b878b14917fe3381a2d74c5685428cc
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-12-19 18:57:22 -0600

    edit conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os, sys
 print(sys.path)
-sys.path.insert(0, os.path.abspath('../../orgroamtools'))
+sys.path.insert(0, os.path.abspath('../orgroamtools'))
 
 project = 'orgroamtools'
 copyright = '2023, abaxi'
@@ -24,6 +24,11 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.githubpages',
               'sphinx_autodoc_typehints']
 
+autodoc_default_options = {
+    "members" : True,
+    "undoc-memebers": True,
+    "private-member" : True
+}
 napoleon_numpy_docstring = True
 templates_path = ['_templates']
 exclude_patterns = []
@@ -35,4 +40,4 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_output_dir = "build/html"
-html_static_path = ['build/html/_static']
+# html_static_path = ['build/html/_static']
