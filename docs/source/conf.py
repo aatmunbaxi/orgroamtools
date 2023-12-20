da17d6765f526eb80f4b304a2dc3cd4aedf5b1b4
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-12-19 21:33:42 -0600

    asldkfj

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+autodoc_mock_imports= ['networkx', 'orgroamtools']
 
 extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.autodoc']
