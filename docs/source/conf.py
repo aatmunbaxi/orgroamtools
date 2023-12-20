2f9966f4d193b8e8be27da6591a6c209e5ef0576
atmnbaxi <abaxi@tamu.edu>
2023-12-20 14:50:23 -0600

    try autodoc mock import variable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,36 +7,55 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os, sys
+
 sys.path.insert(0, os.path.abspath("../../orgroamtools"))
 sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("."))
 
-project = 'orgroamtools'
-copyright = '2023, abaxi'
-author = 'abaxi'
-release = '0.0.1'
+project = "orgroamtools"
+copyright = "2023, abaxi"
+author = "abaxi"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 # autodoc_mock_imports= ['networkx', 'orgroamtools']
+autodoc_mock_imports = [
+    "networkx",
+    "re",
+    "os",
+    "orgroamtools",
+    "sqlite3",
+    "copy",
+    "typing",
+    "dataclasses",
+]
 from unittest.mock import MagicMock
-mock_modules = ['networkx', 'enum', 'dataclasses',
-                'typing','__future__', 'os', 're', 'warnings',
-                'sqlite3', 'copy']
+
+mock_modules = [
+    "networkx",
+    "enum",
+    "dataclasses",
+    "typing",
+    "__future__",
+    "os",
+    "re",
+    "warnings",
+    "sqlite3",
+    "copy",
+]
 # for name in mock_modules:
 #     sys.modules[name] = MagicMock()
 
-extensions = ['sphinx.ext.napoleon',
-              'sphinx.ext.autodoc']
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
