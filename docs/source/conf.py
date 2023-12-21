99c0af18997700f4ea061d3b15b93169b1d0125a
atmnbaxi <abaxi@tamu.edu>
2023-12-20 22:26:58 -0600

    sdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os, sys
 
-sys.path.insert(0, os.path.abspath("../../orgroamtools/"))
+sys.path.insert(0, os.path.abspath("../../"))
 # sys.path.insert(0, os.path.abspath("../.."))
 # sys.path.insert(0, os.path.abspath("../"))
 # sys.path.insert(0, os.path.abspath("."))
